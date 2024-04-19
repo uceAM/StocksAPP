eab164c99109cb0795681520e5452cb5e8dd8b9d
uceAM <arshadm.2406@gmail.com>
2024-04-19 20:13:10 +0530

    changes remaining props from type to interface for consistency

--- frontend/src/Components/IncomeStatements/IncomeStatements.tsx
+++ frontend/src/Components/IncomeStatements/IncomeStatements.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-type Props = {}
+interface Props {}
 
 const IncomeStatements = (props: Props) => {
   return (
--- frontend/src/Components/Sidebar/Sidebar.tsx
+++ frontend/src/Components/Sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-type Props = {}
+interface Props {}
 
 const Sidebar = (props: Props) => {
   return (
