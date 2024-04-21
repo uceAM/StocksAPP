ef4821e23a7d6b28d503f879b2c2a476bd313691
uceAM <arshadm.2406@gmail.com>
2024-04-21 20:36:55 +0530

    adds spinner to SearchPage

--- frontend/src/Pages/SearchPage/SearchPage.tsx
+++ frontend/src/Pages/SearchPage/SearchPage.tsx
@@ -1,47 +1,70 @@
-import { SyntheticEvent, useState } from 'react';
-import CardList from '../../Components/CardList/CardList';
-import Search from '../../Components/Search/Search';
-import { CompanySearch } from '../../company';
-import { searchCompanies } from '../../api';
-import ListPortfolio from '../../Components/Portfolio/ListPortfolio/ListPortfolio';
+import { SyntheticEvent, useState } from "react";
+import CardList from "../../Components/CardList/CardList";
+import Search from "../../Components/Search/Search";
+import { CompanySearch } from "../../company";
+import { searchCompanies } from "../../api";
+import ListPortfolio from "../../Components/Portfolio/ListPortfolio/ListPortfolio";
+import Spinner from "../../Components/Spinner/Spinner";
 
 function SearchPage() {
-  const [search,setSearch] = useState<string>("");
-  const [searchResult,setSearchResult] = useState<CompanySearch[]>([])
-  const [serverError,setServerError] = useState<string>("");
-  const [portfolioValues,setPortfolioValues] = useState<string[]>([]);
-  const getSearchData =(val:string)=>{
-      setSearch(val);
-      // clickMe;
-      // console.log(val);
+  const[searching, setSearching]= useState<boolean>(false);
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>("");
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
+  const getSearchData = (val: string) => {
+    setSearch(val);
+    // clickMe;
+    // console.log(val);
   };
-  const onSearchSubmit =  async (e:SyntheticEvent) =>{
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    setSearching(true);
     e.preventDefault();
     const result = await searchCompanies(search);
-    if (typeof result === 'string'){
+    if (typeof result === "string") {
       setServerError(result);
-    } else if (Array.isArray(result.data)){
+    } else if (Array.isArray(result.data)) {
       setSearchResult(result.data);
+      setSearching(false);
       setServerError("");
       console.log(searchResult);
     }
   };
-  const onPortfolioCreate = (e:any)=> {
+  const onPortfolioCreate = (e: any) => {
     e.preventDefault();
-    let exists =portfolioValues.find(value=> value === e.target[0].value)
-    if(exists) return;
-    setPortfolioValues([...portfolioValues,e.target[0].value]);
-  }
-  const onPortfolioDelete = (e:any) => {
-    e.preventDefault();
-    setPortfolioValues(portfolioValues.filter(value=>value !== e.target[0].value));
+    let exists = portfolioValues.find((value) => value === e.target[0].value);
+    if (exists) return;
+    setPortfolioValues([...portfolioValues, e.target[0].value]);
   };
-  return <>
-    <Search onSearchSubmit = {onSearchSubmit} search = {search} getSearchData ={getSearchData}/>
-    <ListPortfolio portfolioValues = {portfolioValues} onPortfolioDelete ={onPortfolioDelete}/>
-    <CardList searchResults ={searchResult} onPortfolioCreate={onPortfolioCreate}/>
-    {serverError && <h1>{serverError}</h1>}
-  </>;
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    setPortfolioValues(
+      portfolioValues.filter((value) => value !== e.target[0].value)
+    );
+  };
+  return (
+    <>
+      <Search
+        onSearchSubmit={onSearchSubmit}
+        search={search}
+        getSearchData={getSearchData}
+      />
+      <ListPortfolio
+        portfolioValues={portfolioValues}
+        onPortfolioDelete={onPortfolioDelete}
+      />
+      {searching ? (
+        <Spinner />
+      ) : (
+        <CardList
+          searchResults={searchResult}
+          onPortfolioCreate={onPortfolioCreate}
+        />
+      )}
+
+      {serverError && <h1>{serverError}</h1>}
+    </>
+  );
 }
 
 export default SearchPage;
