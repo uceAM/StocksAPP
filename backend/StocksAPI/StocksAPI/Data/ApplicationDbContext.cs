18b3e64cb7ccda408a4a9384895d69fa1485f57f
uceAM <arshadm.2406@gmail.com>
2024-04-23 02:47:47 +0530

    Resolves namespace issue

--- backend/StocksAPI/StocksAPI/Data/ApplicationDbContext.cs
+++ backend/StocksAPI/StocksAPI/Data/ApplicationDbContext.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using StocksAPI.Models;
 using System;
-namespace StocksAPI.Data;
 
 
-public class ApplicationDbContext:DbContext
+
+namespace StocksAPI.Data
 {
-	public ApplicationDbContext(DbContextOptions dbContextOptions) :base(dbContextOptions)
-	{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
 
-	}
-	public DbSet<Stock> Stock { get; set; }
-	public DbSet<Comment> Comment { get; set; }
+        }
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+    }
 }
