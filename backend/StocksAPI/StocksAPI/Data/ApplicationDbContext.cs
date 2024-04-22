bb4ceed14ab8dcdca90ab3ed56a10282c41a9b11
uceAM <arshadm.2406@gmail.com>
2024-04-22 20:16:27 +0530

    Sets up DB, adds Stock and Comment models

---
+++ backend/StocksAPI/StocksAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StocksAPI.Models;
+using System;
+namespace StocksAPI.Data;
+
+
+public class ApplicationDbContext:DbContext
+{
+	public ApplicationDbContext(DbContextOptions dbContextOptions) :base(dbContextOptions)
+	{
+
+	}
+	public DbSet<Stock> Stock { get; set; }
+	public DbSet<Comment> Comment { get; set; }
+}
