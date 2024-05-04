dfecec6bb37e3ebe1a8129eeb37495da7369e13e
uceAM <arshadm.2406@gmail.com>
2024-05-04 11:54:43 +0530

    adds sample user roles to DB

--- backend/StocksAPI/StocksAPI/Data/ApplicationDbContext.cs
+++ backend/StocksAPI/StocksAPI/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using StocksAPI.Models;
 using System;
@@ -12,6 +13,25 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
     {
 
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        List<IdentityRole> roles = new()
+        {
+            new IdentityRole() //standard syntax for creating objects
+            {
+                Name = "User",
+                NormalizedName = "USER",
+            },
+            new IdentityRole //object initiator no paranthesis
+            {
+                Name = "admin",
+                NormalizedName = "ADMIN",
+            }
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
     }
     public DbSet<Stock> Stock { get; set; }
     public DbSet<Comment> Comment { get; set; }
