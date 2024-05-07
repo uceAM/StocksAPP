ac771e833366ea530ad056048016e41a14f06984
uceAM <arshadm.2406@gmail.com>
2024-05-07 15:16:41 +0530

    adds portfolio controller and get method

---
+++ backend/StocksAPI/StocksAPI/Controllers/PortfolioController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StocksAPI.Extensions;
+using StocksAPI.Interfaces;
+using StocksAPI.Models;
+
+namespace StocksAPI.Controllers;
+
+[Route("api/portfolio")]
+[ApiController]
+public class PortfolioController : ControllerBase
+{
+    private readonly UserManager<WebUser> _userManager;
+    private readonly IPortfolioService _portfolioService;
+    public PortfolioController(UserManager<WebUser> userManager, IPortfolioService portfolioService)
+    {
+        _userManager = userManager;
+        _portfolioService = portfolioService;
+    }
+    [HttpGet]
+    [Authorize]
+    public async Task<IActionResult> GetUserPortfolio()
+    {
+        string? username = User.GetUsername();
+        if (string.IsNullOrEmpty(username))
+        {
+            return Unauthorized("username not found");
+        }
+        var user = await _userManager.FindByNameAsync(username);
+        if (user == null)
+        {
+            return BadRequest("User not found");
+        }
+        var userPortfolio = await _portfolioService.GetPortfolio(user);
+        return Ok(userPortfolio);
+    }
+}
---
+++ backend/StocksAPI/StocksAPI/Extensions/ClaimsExtension.cs
@@ -0,0 +1,14 @@
+﻿using StocksAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace StocksAPI.Extensions;
+
+public static class ClaimsExtension
+{
+    public static string? GetUsername(this ClaimsPrincipal user)
+    {
+        return user.FindFirst(x => x.Type.Equals(JwtRegisteredClaimNames.Name)).Value.ToString();
+ 
+    }
+}
